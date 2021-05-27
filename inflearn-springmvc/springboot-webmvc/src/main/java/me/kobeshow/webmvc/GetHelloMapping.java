2699d703835e0b28193a020d402ab428695d73cc
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-05-27 23:08:45 +0900

    공부중 수정

--- inflearn-springmvc/springboot-webmvc/src/main/java/me/kobeshow/webmvc/GetHelloMapping.java
+++ inflearn-springmvc/springboot-webmvc/src/main/java/me/kobeshow/webmvc/GetHelloMapping.java
@@ -3,9 +3,10 @@ package me.kobeshow.webmvc;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
+@Documented
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @RequestMapping(method = RequestMethod.GET, value = "/hello")
 public @interface GetHelloMapping {
--- inflearn-springmvc/springboot-webmvc/src/main/java/me/kobeshow/webmvc/SampleController.java
+++ inflearn-springmvc/springboot-webmvc/src/main/java/me/kobeshow/webmvc/SampleController.java
@@ -12,36 +12,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 //@RequestMapping("/hello")
 public class SampleController {
 
-//    @RequestMapping(value = "/hello",
-//            consumes = MediaType.APPLICATION_JSON_VALUE,
-//            produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @GetHelloMapping
     @ResponseBody
     public String hello() {
         return "hello";
     }
 
-    @RequestMapping(value = "/header", method = RequestMethod.GET, headers = HttpHeaders.AUTHORIZATION)
+    @GetMapping(value = "/header", headers = HttpHeaders.AUTHORIZATION)
     public String header() {
         return "header";
     }
 
-    @RequestMapping(value = "/header2", method = RequestMethod.GET, headers = "!" + HttpHeaders.AUTHORIZATION)
+    @GetMapping(value = "/header2", headers = "!" + HttpHeaders.AUTHORIZATION)
     public String header2() {
         return "header2";
     }
 
-    @RequestMapping(value = "/header2", method = RequestMethod.GET, headers = HttpHeaders.AUTHORIZATION + "=" + "111")
+    @GetMapping(value = "/header2", headers = HttpHeaders.AUTHORIZATION + "=" + "111")
     public String header3() {
         return "header3";
     }
 
-    @RequestMapping(value = "/param", method = RequestMethod.GET, params = "name")
+    @GetMapping(value = "/param", params = "name")
     public String param() {
         return "param";
     }
 
-    @RequestMapping(value = "/param2", method = RequestMethod.GET, params = "name=spring")
+    @GetMapping(value = "/param2", params = "name=spring")
     public String param2() {
         return "param2";
     }
