36b9a653e4f2c78911f0d205caaf6e7d955a4e5e
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-06-12 22:38:18 +0900

    JPA 실전 조회성능 최적화 학습

--- inflearn-springboot-uses/springboot-jpashop/src/main/java/me/kobeshow/springbootjpashop/SpringbootJpashopApplication.java
+++ inflearn-springboot-uses/springboot-jpashop/src/main/java/me/kobeshow/springbootjpashop/SpringbootJpashopApplication.java
@@ -1,7 +1,9 @@
 package me.kobeshow.springbootjpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootJpashopApplication {
@@ -10,4 +12,10 @@ public class SpringbootJpashopApplication {
         SpringApplication.run(SpringbootJpashopApplication.class, args);
     }
 
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
 }
---
+++ inflearn-springboot-uses/springboot-jpashop/src/main/java/me/kobeshow/springbootjpashop/repository/OrderSimpleQueryDto.java
@@ -0,0 +1,24 @@
+package me.kobeshow.springbootjpashop.repository;
+
+import lombok.Data;
+import me.kobeshow.springbootjpashop.domain.Address;
+import me.kobeshow.springbootjpashop.domain.OrderStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name; // LAZY 초기화
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address; // LAZY 초기화
+    }
+}
