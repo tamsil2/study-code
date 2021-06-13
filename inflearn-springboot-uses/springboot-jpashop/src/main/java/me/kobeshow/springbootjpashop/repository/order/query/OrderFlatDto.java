5c45d4dfe06642c7a4075c1b17ab48996484aa5b
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-06-13 21:55:47 +0900

    API 개발 컬렉션조회 최적화 학습

---
+++ inflearn-springboot-uses/springboot-jpashop/src/main/java/me/kobeshow/springbootjpashop/repository/order/query/OrderFlatDto.java
@@ -0,0 +1,32 @@
+package me.kobeshow.springbootjpashop.repository.order.query;
+
+import lombok.Data;
+import me.kobeshow.springbootjpashop.domain.Address;
+import me.kobeshow.springbootjpashop.domain.OrderStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderFlatDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+}
