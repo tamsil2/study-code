b163a252e16b092a9c427b5aa13f49583bc3eb1e
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-08-11 23:24:37 +0900

    Spring MVC 상품목록 - 타임리프

---
+++ spring-mvc-kim/item-service/src/main/java/me/tamsil/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,33 @@
+package me.tamsil.itemservice.web.basic;
+
+import lombok.RequiredArgsConstructor;
+import me.tamsil.itemservice.domain.item.Item;
+import me.tamsil.itemservice.domain.item.ItemRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
