7d2cd41e6d24d0db10d8103a128e385c7d5c2bf8
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-11-06 08:23:33 +0900

    디자인 패턴 - 데코레이터 패턴

---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_02_structural_patterns/_09_decorator/_01_before/TrimmingCommentService.java
@@ -0,0 +1,12 @@
+package me.tamsil.designpatterns._02_structural_patterns._09_decorator._01_before;
+
+public class TrimmingCommentService extends CommentService{
+    @Override
+    public void addComment(String comment) {
+        super.addComment(trim(comment));
+    }
+
+    private String trim(String comment) {
+        return comment.replace("...", "");
+    }
+}
---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_02_structural_patterns/_09_decorator/_02_after/App.java
@@ -0,0 +1,23 @@
+package me.tamsil.designpatterns._02_structural_patterns._09_decorator._02_after;
+
+public class App {
+    private static boolean enabledSpamFilter = false;
+    private static boolean enabledTrimFilter = true;
+
+    public static void main(String[] args) {
+        CommentService commentService = new DefaultCommentService();
+
+        if (enabledSpamFilter) {
+            commentService = new SpamFilteringCommentDecorator(commentService);
+        }
+
+        if (enabledTrimFilter) {
+            commentService = new TrimmingCommentDecorator(commentService);
+        }
+
+        Client client = new Client(commentService);
+        client.writeComment("오징어게임");
+        client.writeComment("보는게 하는거 보다 재밌을 수가 없지...");
+        client.writeComment("http://tamsil.me");
+    }
+}
