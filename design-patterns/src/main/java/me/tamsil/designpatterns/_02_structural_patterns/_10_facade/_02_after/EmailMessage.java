99297ee8e9191c9dd55ffd6c6f6e2b2f1e0f74dd
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-11-06 21:36:32 +0900

    디자인 패턴 - 퍼사드 패턴

---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_02_structural_patterns/_10_facade/_02_after/EmailMessage.java
@@ -0,0 +1,58 @@
+package me.tamsil.designpatterns._02_structural_patterns._10_facade._02_after;
+
+public class EmailMessage {
+    private String from;
+    private String to;
+    private String subject;
+    private String text;
+    private String cc;
+    private String bcc;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+
+    public String getBcc() {
+        return bcc;
+    }
+
+    public void setBcc(String bcc) {
+        this.bcc = bcc;
+    }
+}
--- thejava/java-bytecode/src/main/java/me/kobeshow/App.java
+++ thejava/java-bytecode/src/main/java/me/kobeshow/App.java
@@ -6,6 +6,9 @@ package me.kobeshow;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
     }
 }
