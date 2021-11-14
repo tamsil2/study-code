fb51abce4b1ee5ab66d9bc9162d611ae338f6777
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-11-14 09:38:11 +0900

    Spring MVC 고급 - 동적프록시기술

---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_03_behavioral_patterns/_14_command/_02_after/MyApp.java
@@ -0,0 +1,19 @@
+package me.tamsil.designpatterns._03_behavioral_patterns._14_command._02_after;
+
+import me.tamsil.designpatterns._03_behavioral_patterns._14_command._01_before.Game;
+
+public class MyApp {
+    private Command command;
+
+    public MyApp(Command command) {
+        this.command = command;
+    }
+
+    public void press() {
+        command.execute();
+    }
+
+    public static void main(String[] args) {
+        MyApp myApp = new MyApp(new GameStartCommand(new Game()));
+    }
+}
