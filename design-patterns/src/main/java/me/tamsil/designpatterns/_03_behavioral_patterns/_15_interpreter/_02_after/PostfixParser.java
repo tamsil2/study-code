bedce48fa3bb388a9c6c471d1be90ef894a86c53
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-11-15 22:43:27 +0900

    디자인 패턴 - 인터프리터 패턴

---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_03_behavioral_patterns/_15_interpreter/_02_after/PostfixParser.java
@@ -0,0 +1,42 @@
+package me.tamsil.designpatterns._03_behavioral_patterns._15_interpreter._02_after;
+
+import java.util.Stack;
+
+import static me.tamsil.designpatterns._03_behavioral_patterns._15_interpreter._02_after.PostfixExpression.*;
+
+public class PostfixParser {
+
+    public static PostfixExpression parse(String expression) {
+        Stack<PostfixExpression> stack = new Stack<>();
+        for (char c : expression.toCharArray()) {
+            stack.push(getExpression(c, stack));
+        }
+        return stack.pop();
+    }
+
+//    private static PostfixExpression getExpression(char c, Stack<PostfixExpression> stack) {
+//        switch (c) {
+//            case '+':
+//                return new PlusExpression(stack.pop(), stack.pop());
+//            case '-':
+//                PostfixExpression right = stack.pop();
+//                PostfixExpression left = stack.pop();
+//                return new MinusExpression(left, right);
+//            default:
+//                return new VariableExpression(c);
+//        }
+//    }
+
+    private static PostfixExpression getExpression(char c, Stack<PostfixExpression> stack) {
+        switch (c) {
+            case '+':
+                return plus(stack.pop(), stack.pop());
+            case '-':
+                PostfixExpression right = stack.pop();
+                PostfixExpression left = stack.pop();
+                return minus(left, right);
+            default:
+                return variable(c);
+        }
+    }
+}
---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_03_behavioral_patterns/_15_interpreter/_03_java/Book.java
@@ -0,0 +1,17 @@
+package me.tamsil.designpatterns._03_behavioral_patterns._15_interpreter._03_java;
+
+public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
