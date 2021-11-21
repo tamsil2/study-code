a56be292e90d2835432c368ac2d9b38af7945818
tamsil2 <46986164+tamsil2@users.noreply.github.com>
2021-11-21 23:25:00 +0900

    스프링 핵심원리 고급 - 스프링 AOP 구현

---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_03_behavioral_patterns/_17_mediator/_01_before/Gym.java
@@ -0,0 +1,9 @@
+package me.tamsil.designpatterns._03_behavioral_patterns._17_mediator._01_before;
+
+public class Gym {
+    private CleaningService cleaningService;
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+}
---
+++ design-patterns/src/main/java/me/tamsil/designpatterns/_03_behavioral_patterns/_17_mediator/_01_before/Hotel.java
@@ -0,0 +1,12 @@
+package me.tamsil.designpatterns._03_behavioral_patterns._17_mediator._01_before;
+
+public class Hotel {
+    public static void main(String[] args) {
+        Guest guest = new Guest();
+        guest.getTower(3);
+        guest.dinner();
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.clean();
+    }
+}
